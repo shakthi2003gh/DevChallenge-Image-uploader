fbc605032fce477834b7e8ee28a7bcb161a4a74d
shakthi <shakthikumar123456789@gmail.com>
2022-11-09 22:24:08 +0530

    Create loading stage

---
+++ src/components/loadingState.jsx
@@ -0,0 +1,10 @@
+const LoadingState = () => {
+  return (
+    <div className="loading-state">
+      <h1>Uploading...</h1>
+      <div className="loading"></div>
+    </div>
+  );
+};
+
+export default LoadingState;
