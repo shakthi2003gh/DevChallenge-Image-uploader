c7afa2799f8f14a3013cd6e714e9237b37ee4a63
shakthi <shakthikumar123456789@gmail.com>
2022-11-10 20:57:08 +0530

    Create image uploader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import UploadState from "./components/uploadState";
+import UploadedState from "./components/uploadedState";
+import LoadingState from "./components/loadingState";
 
 function App() {
-  return <UploadState />;
+  const [fileName, setFileName] = useState("");
+  const [uploadedStage, setUploadedStage] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const uploadProps = { setFileName, setUploadedStage, setLoading };
+  const uploadedProps = { fileName };
+
+  return (
+    <>
+      {!uploadedStage && !loading && <UploadState {...uploadProps} />}
+      {loading && <LoadingState />}
+      {uploadedStage && !loading && <UploadedState {...uploadedProps} />}
+    </>
+  );
 }
 
 export default App;
